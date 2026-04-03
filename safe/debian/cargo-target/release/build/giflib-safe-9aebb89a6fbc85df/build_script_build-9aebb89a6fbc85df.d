2d8d9d31ebd6f15087953f0da9e174922fcaa859
Yan <yans@yancomm.net>
2026-04-03 09:40:15 -0700

    Remove generated Debian build outputs

--- safe/debian/cargo-target/release/build/giflib-safe-9aebb89a6fbc85df/build_script_build-9aebb89a6fbc85df.d
+++
@@ -1,5 +0,0 @@
-/home/yans/code/safelibs/ported/giflib/safe/debian/cargo-target/release/build/giflib-safe-9aebb89a6fbc85df/build_script_build-9aebb89a6fbc85df.d: build.rs
-
-/home/yans/code/safelibs/ported/giflib/safe/debian/cargo-target/release/build/giflib-safe-9aebb89a6fbc85df/build_script_build-9aebb89a6fbc85df: build.rs
-
-build.rs:
--- safe/debian/cargo-target/release/build/libc-7525f266778fb6d9/build_script_build-7525f266778fb6d9.d
+++
@@ -1,5 +0,0 @@
-/home/yans/code/safelibs/ported/giflib/safe/debian/cargo-target/release/build/libc-7525f266778fb6d9/build_script_build-7525f266778fb6d9.d: /home/yans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.184/build.rs
-
-/home/yans/code/safelibs/ported/giflib/safe/debian/cargo-target/release/build/libc-7525f266778fb6d9/build_script_build-7525f266778fb6d9: /home/yans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.184/build.rs
-
-/home/yans/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.184/build.rs:
